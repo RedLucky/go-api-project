7493b583c54620de60323c19ccee44ecb686796c
lucky fernanda <luckyprojec@gmail.com>
2021-08-19 21:48:44 +0700

    add generated links

--- app/delivery/api/auth/token.go
+++ app/delivery/api/auth/token.go
@@ -1,12 +1,8 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -56,38 +52,4 @@ func ExtractToken(r *http.Request) string {
 		return strings.Split(bearerToken, " ")[1]
 	}
 	return ""
-}
-
-func ExtractTokenID(r *http.Request) (uint32, error) {
-
-	tokenString := ExtractToken(r)
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(os.Getenv("API_SECRET")), nil
-	})
-	if err != nil {
-		return 0, err
-	}
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if ok && token.Valid {
-		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
-		if err != nil {
-			return 0, err
-		}
-		return uint32(uid), nil
-	}
-	return 0, nil
-}
-
-//Pretty display the claims licely in the terminal
-func Pretty(data interface{}) {
-	b, err := json.MarshalIndent(data, "", " ")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	fmt.Println(string(b))
 }
---
+++ domain/generated_link.go
@@ -0,0 +1,11 @@
+package domain
+
+// define models
+type GeneratedLinks struct {
+}
+
+type GeneratedLinkUsecase interface {
+}
+
+type GeneratedLinkRepository interface {
+}
